48338f25e5de4fb0100cd321c7d927f3d7998430
shoobah <shoobah@gmail.com>
2015-07-29 02:33:04 +0000

    Input degrees

--- motor1/turn1.js
+++ motor1/turn1.js
@@ -15,17 +15,43 @@ var seq = [
   [1, 0, 0, 1]
 ]
 
-var currentStep = 0
+var seq2 = [
+  [1, 1, 0, 0],
+  [0, 1, 1, 0],
+  [0, 0, 1, 1],
+  [1, 0, 0, 1]
+]
 
-function step () {
-  if (currentStep >= seq.length) {
+var currentStep = 0
+var count = 0
+
+function step (seqToUse, numSteps, increment) {
+  if (currentStep >= seqToUse.length) {
     currentStep = 0
   }
-  in1.writeSync(seq[currentStep][0])
-  in2.writeSync(seq[currentStep][1])
-  in3.writeSync(seq[currentStep][2])
-  in4.writeSync(seq[currentStep][3])
-  currentStep += 1
+  if (currentStep < 0) {
+    currentStep = seqToUse.length - 1
+  }
+  in1.writeSync(seqToUse[currentStep][0])
+  in2.writeSync(seqToUse[currentStep][1])
+  in3.writeSync(seqToUse[currentStep][2])
+  in4.writeSync(seqToUse[currentStep][3])
+  currentStep += increment
+  count += 1
+  if (!numSteps) {
+    numSteps = 4096
+  }
+  if (count > numSteps) {
+    clearInterval(stepper)
+  }
 }
 
-setInterval(step, 1)
+var stepsPerDegree = 4096 / 360
+var maxSteps = process.argv[2] * stepsPerDegree
+var increment = 1
+if (maxSteps < 0) {
+  maxSteps = maxSteps * -1
+  increment = -1
+}
+console.log('Steps:', maxSteps)
+var stepper = setInterval(function () {step(seq, maxSteps, increment)}, 1)
